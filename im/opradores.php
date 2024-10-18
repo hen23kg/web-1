31eab460f06becb5d54fc9eda5462ae926685b63
hen23kg <142592934+hen23kg@users.noreply.github.com>
2024-10-18 16:45:41 -0400

    Add files via upload

---
+++ im/opradores.php
@@ -0,0 +1,10 @@
+<?php
+//oparedores nuveo archivo
+//pi contante
+// y almacena $area = pi *$radio *$radio;
+echo"operadores matematicos";
+const pi = 546.56;
+$radio = 5;
+$area = pi *$radio *$radio;
+echo "el area del circulo es:",$area;
+?>
---
+++ im/variable.php
@@ -0,0 +1,16 @@
+<?php
+$nombre='luis';
+echo"la variable nombre es de tipo:",gettype($nombre);
+
+const=434.43;
+//contante que no va acambiar y no requiere modificacion
+
+//oparedores nuveo archivo
+//pi contante
+// y almacena $area = pi *$radio *$radio;
+echo"operadores matematicos";
+const pi = 546.56;
+$radio = 5;
+$area = pi *$radio *$radio;
+echo "el area del circulo es:",$area;
+?>
